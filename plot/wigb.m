f22a864151ee7f510db9d7e39db9202dacec1770
nicklinyi <linyicas@qq.com>
2018-04-27 22:34:48 +0800

    fix a bug in wigb.m

--- plot/wigb.m
+++ plot/wigb.m
@@ -1,7 +1,7 @@
 function wigb(a,scal,x,z,amx)
 %WIGB: Plot seismic data using wiggles.
 %
-%  WIGB(a,scal,x,z,amx) 
+%  WIGB(a,scal,x,z,amx)
 %
 %  IN  a:     seismic data (a matrix, traces are columns)
 %      scale: multiple data by scal
@@ -10,15 +10,19 @@ function wigb(a,scal,x,z,amx)
 %
 %  Note
 %
-%    If only 'a' is enter, 'scal,x,z,amn,amx' are set automatically; 
-%    otherwise, 'scal' is a scalar; 'x, z' are vectors for annotation in 
+%    If only 'a' is enter, 'scal,x,z,amn,amx' are set automatically;
+%    otherwise, 'scal' is a scalar; 'x, z' are vectors for annotation in
 %    offset and time, amx are the amplitude range.
 %
 %
 %  Author(s): Xingong Li (Intseis, Integrated Seismic Solutions)
-%  Copyright 1998-2003 Xingong 
-%  Revision: 1.2  Date: Dec/2002 
-%  
+%  Copyright 1998-2003 Xingong
+%  Revision: 1.2  Date: Dec/2002
+%  Copyright 2018 Yi Lin
+%  Revision: 1.3  Date: April/2018
+%  fix a bug: when a trace has its values being all negative and the same, and
+% also very small, e.g. trace(:,17) = -1.39e-17, the original code fails. I fixed
+% this bug by simply modify the line 75.
 
  if nargin == 0, nx=10;nz=10; a = rand(nz,nx)-0.5; end;
 
@@ -38,25 +42,25 @@ function wigb(a,scal,x,z,amx)
 
 
  dz=z(2)-z(1);
- xmx=max(max(a)); xmn=min(min(a)); 
+ xmx=max(max(a)); xmn=min(min(a));
 
  if scal == 0; scal=1; end;
- a = a * dx /amx; 
+ a = a * dx /amx;
  a = a * scal;
 
  fprintf(' PlotWig: data range [%f, %f], plotted max %f \n',xmn,xmx,amx);
- 
-% set display range 
+
+% set display range
 
  x1=min(x)-2.0*dx; x2=max(x)+2.0*dx;
  z1=min(z)-dz; z2=max(z)+dz;
- 
+
  set(gca,'NextPlot','add','Box','on', ...
   'XLim', [x1 x2], ...
   'YDir','reverse', ...
   'YLim',[z1 z2]);
 
- 
+
 
 	fillcolor = [0 0 0];
 	linecolor = [0 0 0];
@@ -67,15 +71,15 @@ function wigb(a,scal,x,z,amx)
 	zend  =z(nz);
 
 for i=1:nx,
-   
-  if trmx(i) ~= 0;    % skip the zero traces
+
+  if trmx(i) ~= 0 && trmx(i) >= 1e-8;    % skip the zero traces, fix the bug 
 	tr=a(:,i); 	% --- one scale for all section
   	s = sign(tr) ;
   	i1= find( s(1:nz-1) ~= s(2:nz) );	% zero crossing points
 	npos = length(i1);
 
 
-	%12/7/97 
+	%12/7/97
 	zadd = i1 + tr(i1) ./ (tr(i1) - tr(i1+1)); %locations with 0 amplitudes
 	aadd = zeros(size(zadd));
 
@@ -88,13 +92,13 @@ for i=1:nx,
 		if tr(1)>0, 	a0=0; z0=1.00;
 		else, 		a0=0; z0=zadd(1);
 		end;
-		if tr(nz)>0, 	a1=0; z1=nz; 
+		if tr(nz)>0, 	a1=0; z1=nz;
 		else, 		a1=0; z1=max(zadd);
 		end;
-			
+
 	zz = [z0; zz; z1; z0];
  	aa = [a0; aa; a1; a0];
-		
+
 	zzz = zstart + zz*dz -dz;
 
 patch( aa+x(i) , zzz,  fillcolor);
